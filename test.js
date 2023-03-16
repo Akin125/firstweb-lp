fbbe5b95c896b67eb383199a22008a582cfa64a1
Odediran Philip Oluseyi <110476898+Akin125@users.noreply.github.com>
2023-03-16 15:40:14 +0100

    nav start

--- test.js
+++
@@ -1,8 +0,0 @@
-let docTitle = document.title;
-window.addEventListener("blur", () => {
-    document.title = "Come Back ):";
-});
-
-window.addEventListener("Focus", () => {
-    document.title = docTitle;
-});
